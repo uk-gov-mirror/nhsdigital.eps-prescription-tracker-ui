45fc622390fd1bac41a24ea34253c1c57722198e
jonathanwelch1-nhs <148754575+jonathanwelch1-nhs@users.noreply.github.com>
2026-04-27 09:45:05 +0100

    Fix: [AEA-6406] - timeout modal sync (#1986)
    ## Summary
    
    https://nhsd-jira.digital.nhs.uk/browse/AEA-6406
    
    - Routine Change
    
    
    ### Details
    Changing the way we handle the timeout modal being counted, now using
    the server time rather than froint end timing which can be affected by
    browser throttling when the tab is minimised

--- packages/common/commonTypes/src/sessionTimeoutModalState.ts
+++ packages/common/commonTypes/src/sessionTimeoutModalState.ts
@@ -1,6 +1,6 @@
 export type SessionTimeoutModal = {
     showModal: boolean
-    timeLeft: number
+    sessionEndTime: number | null
     buttonDisabled: boolean
     action: "extending" | "loggingOut" | undefined
 }
